7a4e0bf6a31f321f2476bb02090b590a964dcf35
Mathieu <mathieuvantuil2000@gmail.com>
2022-12-14 10:26:38 +0100

    workshop2module4

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+document.getElementById("c").onclick = function(){ // als op de c-toets wordt geklikt
+	var c_toets = document.getElementById("audio_c"); // zoek het c-toets audio-element op
+	c_toets.play(); // speel het audio-element af
+}
+document.getElementById("d").onclick = function(){
+	var d_toets = document.getElementById("audio_d");
+	d_toets.play();
+}
+document.getElementById("e").onclick = function(){
+	var e_toets = document.getElementById("audio_e");
+	e_toets.play();
+}
+document.getElementById("144").onclick = function(){
+	var woof_toets = document.getElementById("audio_144");
+	woof_toets.play();
+}
+document.getElementById("220").onclick = function(){
+	var dub_toets = document.getElementById("audio_220");
+	dub_toets.play();
+}
+document.getElementById("362").onclick = function(){
+	var meow_toets = document.getElementById("audio_362");
+	meow_toets.play();
+}
+document.getElementById("392").onclick = function(){
+	var scream_toets = document.getElementById("audio_392");
+	scream_toets.play();
+}
+document.getElementById("426").onclick = function(){
+	var beep_toets = document.getElementById("audio_426");
+	beep_toets.play();
+}
+document.getElementById("445").onclick = function(){
+	var zombie_toets = document.getElementById("audio_445");
+	zombie_toets.play();
+}
+document.getElementById("575").onclick = function(){
+	var radar_toets = document.getElementById("audio_575");
+	radar_toets.play();
+}
